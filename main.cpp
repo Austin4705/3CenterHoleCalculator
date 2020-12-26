0307fdb5bdcc0d41850881bd90b1d5c22bdae716
Austin4705 <35353582+Austin4705@users.noreply.github.com>
2020-12-26 13:39:35 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+typedef const long double constParameter;
+typedef long double parameter;
+
+//change these to edit the output of the script
+constParameter bottomLength = 45.0; //aka c2c distance
+constParameter backHubWheelRadius = 38.2;
+constParameter topHubWheelRadius = backHubWheelRadius;
+constParameter length = 45.0;
+
+//function headers
+parameter calculateArc(parameter degrees, parameter diameter);
+
+int main() {
+    parameter totalLength = bottomLength + (2 * sideLength);//calculates total length of non-degree related stuff
+}
+
+parameter calculateArc(parameter degrees, parameter diameter){
+    return 0;
+}
+
